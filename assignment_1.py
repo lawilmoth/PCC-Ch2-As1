e970e58a747167ff2f019a5b3ad3d20954a25f81
= <=>
2024-08-27 22:14:28 -0400

    Added questions and tests for the first assignment

--- assignment_1.py
+++ assignment_1.py
@@ -1,5 +1,55 @@
+"""For this assignment, you will need to follow the directions for each of the examples functions below."""
+
 
 def hello_world():
-    print("hello")
+    """This problem should print hello
+    To solve this
+    1. Delete the word pass.
+    2. Uncomment the print statement"""
+    pass
+    #print("hello")
 
 
+#This calls the function to test if it is working. You can delete it when you are done testing it, and call the function you are working on.
+hello_world()
+
+def problem_one():
+    """Create a variable called 'name' and set it equal to your name,
+    then print 'Hello <name>' 
+    be sure to delete the pass."""
+    name = ""
+
+    return name
+
+def problem_two():
+    """Use the strip method remove the whitespace """
+    x = "                               \t\t          \n          <( o-o )>\n\n\t\t\t          \t"
+
+    #Add the left strip to remove whitespace on the left
+    ls = x
+    print(ls)
+    #Add the right strip to remove whitespace on the right
+    rs = x
+    print(rs)
+    #Add a method to remove all whitespace
+    s = x
+    print(s)
+    return x, ls, rs, s
+
+
+def problem_three():
+    """Use the appropriate string methods to solve the problems"""
+    x = "WE ARE the KNights WHO SAY ni!"
+
+    #Use the appropriate method to write this in all capital letters
+    caps = x
+    print(caps)
+
+    #Use the appropriate method to write this in all lowercase letters
+    lower = x
+    print(lower)
+    #Use the appropriate method to write this in title case
+    title_case = x
+    print(title_case)
+
+    return x, caps, lower, title_case
--- test_assignment_1.py
+++ test_assignment_1.py
@@ -1,26 +1,65 @@
 import unittest
-import io, sys
+from cpo import capture_print_output 
 
 from assignment_1 import *
 
-def capture_print_output(func):
-    def inner(*args, **kwargs):
-        out = io.StringIO()
-        sys.stdout = out
-        func(*args, **kwargs)
-        sys.stdout = sys.__stdout__
-        return out.getvalue().strip()
-    return inner
-
-
-
 
 class TestHello(unittest.TestCase):
-    def test_print(self):
-        capture_print_output
+    def test_print_hello(self):
+        #capture_print_output
         ans = capture_print_output(hello_world)()
-        print(ans)
+        
         self.assertTrue(ans =="hello", "You did not print 'hello'.")
+
+class TestProblem1(unittest.TestCase):
+    def test_start_with_hello(self):
+
+        ans = capture_print_output(problem_one)()
+        
+        self.assertTrue(ans[0:5] =="Hello", "The output should start by writing 'Hello'")
+    def test_ends_with_name (self):
+
+        ans = capture_print_output(problem_one)()
+        
+        self.assertGreater(ans, "Hello", "Be sure to output more than just hello")
+    def test_hello_name (self):
+
+        ans = capture_print_output(problem_one)()
+        var = problem_one()
+        self.assertEqual(ans, f"Hello {var}", "The output should say Hello plus your name")
+
+class TestProblem2(unittest.TestCase):
+    
+    def test_x_wasnt_changed(self):
+
+        x, ls, rs, s = problem_two()
+        self.assertEqual(
+            x, "                               \t\t          \n          <( o-o )>\n\n\t\t\t          \t", "Don't modify 'x'"
+            )
+        
+    def test_ls (self):
+        x, ls, rs, s = problem_two()
+        self.assertEqual(
+            ls, "<( o-o )>\n\n\t\t\t          \t", "use lstrip()"
+            )
+        
+    def test_rs (self):
+        x, ls, rs, s = problem_two()
+        self.assertEqual(
+            rs, "                               \t\t          \n          <( o-o )>", "use rstrip()"
+            )
+        
+    def test_s (self):
+        x, ls, rs, s = problem_two()
+        self.assertEqual(
+            s, "<( o-o )>", "use the normal strip method"
+            )
+        
+        
+        
+        
+
+
 
 if __name__ == "__main__":
     unittest.main()
